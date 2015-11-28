ccc66f911d9386b97fe869b64d18b4a91434434c
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-11-28 16:54:39 +0000

    add betfair pem to env var

--- betfair_tennis_api/__init__.py
+++ betfair_tennis_api/__init__.py
@@ -2,13 +2,20 @@ from flask import Flask
 from flask.ext.cache import Cache
 from betfair import Betfair
 from betfair.utils import BetfairEncoder
+from cStringIO import StringIO
+import os
+
 
 application = app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('APP_CONFIG_FILE')
 app.json_encoder = BetfairEncoder
 
-client = Betfair(app.config['BETFAIR_APPLICATION_KEY'], './instance/betfair.pem')
+f = open('betfair.pem', 'w+')
+f.write(os.environ['testvar'])
+f.close()
+
+client = Betfair(app.config['BETFAIR_APPLICATION_KEY'], 'betfair.pem')
 client.login(app.config['BETFAIR_USER_NAME'], app.config['BETFAIR_PASSWORD'])
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
