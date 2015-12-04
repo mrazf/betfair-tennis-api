28e159e35ec5859abae5add48cecfb689a970e99
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-12-04 01:05:01 +0000

    config restructuring

--- betfair_tennis_api/__init__.py
+++ betfair_tennis_api/__init__.py
@@ -12,11 +12,11 @@ app.config.from_object(__name__)
 app.config.from_envvar('APP_CONFIG_FILE')
 app.json_encoder = BetfairEncoder
 
-f = open('betfair.pem', 'w+')
-f.write(os.environ['PEM_FILE'])
+f = open('./config/betfair.pem', 'w+')
+f.write(app.config['PEM_FILE'])
 f.close()
 
-client = Betfair(app.config['BETFAIR_APPLICATION_KEY'], 'betfair.pem')
+client = Betfair(app.config['BETFAIR_APPLICATION_KEY'], './config/betfair.pem')
 client.login(app.config['BETFAIR_USER_NAME'], app.config['BETFAIR_PASSWORD'])
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
--- betfair_tennis_api/views.py
+++ betfair_tennis_api/views.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from betfair_tennis_api import app
 from dao import site_navigation
 import matches
+import match_path
 
 
 @app.route("/betfair-tennis-api/tennisNavigation/")
@@ -19,3 +20,10 @@ def tennis_match(event_id):
     events = site_navigation.get_tennis_event(event_id)
 
     return jsonify(market_catalogue=events)
+
+
+@app.route("/betfair-tennis-api/rawMatchPaths")
+def _match_path():
+    tennis_matches = site_navigation.get_tennis_navigation()
+
+    return jsonify(rawMatchPaths=match_path.get(tennis_matches))
