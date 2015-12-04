a3ad622020b3e7c168994edba3c999571dd72c87
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-12-04 11:44:07 +0000

    PEP8 cleaning

--- betfair_tennis_api/match_path/__init__.py
+++ betfair_tennis_api/match_path/__init__.py
@@ -1,8 +1,6 @@
-import json
-
-
 def recursive(root):
     paths = []
+
     def inner(root, path, depth):
         if 'marketType' in root and root['marketType'] == "MATCH_ODDS":
             paths.append(dict(path))
@@ -11,7 +9,8 @@ def recursive(root):
         if 'children' in root:
             for child in root['children']:
                 inner(child, path, depth+1)
-                if path: path.pop(depth+1)
+                if path:
+                    path.pop(depth+1)
 
     inner(root, {}, 0)
     return paths
