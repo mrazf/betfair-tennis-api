0404b95018d037509f7f429eb63fd3142d4447e3
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-12-07 23:00:42 +0000

    Rename views to endpoints

--- betfair_tennis_api/__init__.py
+++ betfair_tennis_api/__init__.py
@@ -27,7 +27,7 @@ CORS(app, origins=['http://localhost:4200'])
 stream_handler = logging.StreamHandler()
 app.logger.addHandler(stream_handler)
 
-import betfair_tennis_api.views
+import betfair_tennis_api.endpoints
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
