fd020aeb32affb76014a1875656b59a921ad5ca6
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2015-12-07 00:26:04 +0000

    changed dict to list

--- betfair_tennis_api/match_path/__init__.py
+++ betfair_tennis_api/match_path/__init__.py
@@ -1,30 +1,31 @@
-# MESSY
-
 def is_match(elem):
     if 'marketType' in elem and elem['marketType'] == "MATCH_ODDS":
         return True
 
     return False
 
+
 def pull_out_matches(elem, path, depth, paths):
-    print path
     if 'children' in elem:
         for child in elem['children']:
+            path.append(elem['name'])
             if is_match(child):
-                path['id'] = elem['id']
-                path[depth] = elem['name']
+                match = {}
+                match['id'] = elem['id']
+                match['path'] = path
 
-                print 'dicting', dict(path)
-                paths.append(dict(path))
+                paths.append(match)
                 continue
 
-            path[depth] = elem['name']
-            pull_out_matches(child, dict(path), depth+1, paths)
+            pull_out_matches(child, list(path), depth+1, paths)
+
+            if path:
+               path.pop()
 
     return paths
 
 
 def get(tennis_matches):
-    paths = pull_out_matches(tennis_matches, {}, 0, [])
+    paths = pull_out_matches(tennis_matches, [], 0, [])
 
     return paths
