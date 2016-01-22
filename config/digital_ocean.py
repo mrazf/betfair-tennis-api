7ff0d5f15808a4bc85a5c8482403727c17d731ac
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2016-01-22 23:50:54 +0000

    Add digital ocean config

---
+++ config/digital_ocean.py
@@ -0,0 +1,8 @@
+import os
+
+BETFAIR_APPLICATION_KEY = os.environ['BETFAIR_APPLICATION_KEY']
+BETFAIR_USER_NAME = os.environ['BETFAIR_USER_NAME']
+BETFAIR_PASSWORD = os.environ['BETFAIR_PASSWORD']
+
+with open('~/betfair.pem', 'r') as pem:
+    PEM_FILE = pem.read()
