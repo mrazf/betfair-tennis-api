1968f138b457c631a3bc08d11266fee4764c8404
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2016-01-23 19:03:00 +0000

    Testing auto deploy

--- config/digital_ocean.py
+++ config/digital_ocean.py
@@ -4,5 +4,5 @@ BETFAIR_APPLICATION_KEY = os.environ['BETFAIR_APPLICATION_KEY']
 BETFAIR_USER_NAME = os.environ['BETFAIR_USER_NAME']
 BETFAIR_PASSWORD = os.environ['BETFAIR_PASSWORD']
 
-with open('~/betfair.pem', 'r') as pem:
+with open('/home/stringer/betfair.pem', 'r') as pem:
     PEM_FILE = pem.read()
--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,16 @@
+import os
 from fabric.api import *
+from contextlib import contextmanager
 
 env.hosts = ["46.101.87.122"]
 env.user = "stringer"
+env.password = os.environ['STRINGER_PASSWORD']
 env.key_filename = "~/.ssh/id_rsa.pub"
 
-def switch_to_virt_env():
-    run("virtualenv env")
-    run("source env/bin/activate")
-
-
-def pip_install():
-    run("pip install -r requirements.txt")
+@contextmanager
+def source_virtualenv():
+    with prefix('source ' + os.path.join("/home/stringer/betfair-tennis-api/env/bin/activate")):
+        yield
 
 
 def deploy():
@@ -19,7 +19,11 @@ def deploy():
         run("rm -rf betfair-tennis-api")
         run("git clone git@github.com:maxfar/betfair-tennis-api.git")
         with cd(code_dir):
+            run("pwd")
             run("virtualenv env")
             run("source env/bin/activate")
-            run("pip install -r requirements.txt")
-            run("sudo restart befair")
+            run("ls")
+            run("which python")
+            with source_virtualenv():
+                run("pip install -r requirements.txt")
+                run("sudo restart betfair")
