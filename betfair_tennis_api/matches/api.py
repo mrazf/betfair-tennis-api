6cf90bfe553397d6a159e112b507f19a7bb05600
Maxwell Farrell <maxwellrichardfarrell@gmail.com>
2016-02-10 00:43:07 +0000

    Add mens and singles filtering to matches

--- betfair_tennis_api/matches/api.py
+++ betfair_tennis_api/matches/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..dao import site_navigation
 import enrich
 import prune_path
@@ -11,5 +11,34 @@ def matches():
     raw_match_paths = site_navigation.get_raw_match_paths()
     enriched_matches = enrich.do(raw_match_paths)
     pruned_matches = prune_path.do(enriched_matches)
+    filtered_matches = _filter(pruned_matches)
 
-    return jsonify(matches=pruned_matches)
+    result = {
+        'matches': filtered_matches,
+        'metadata': {
+            'matchCount': len(raw_match_paths),
+            'filteredMatchCount': len(filtered_matches)
+        }
+    }
+
+    return jsonify(result)
+
+
+def _filter(matches):
+    mens = request.args.get('mens')
+    if mens:
+        mens = req_bool_conversion(mens)
+        matches = filter(lambda m: m['mens'] == mens, matches)
+
+    singles = request.args.get('singles')
+    if singles:
+        singles = req_bool_conversion(singles)
+        matches = filter(lambda m: m['singles'] == singles, matches)
+
+    return matches
+
+
+def req_bool_conversion(string):
+    if string == "true":
+        return True
+    return False
